bdfa5951068e7bc57bad1090cffb6d931574a408
fdocharles <fdocharles@gmail.com>
2022-01-31 17:55:26 -0500

    Initial project

---
+++ src/components/ItemCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="col-sm-6 col-lg-4 mb-4 item-card" data-aos="fade-up">
+    <div class="block-4">
+      <figure class="block-4-image">
+        <a href="shop-single.html"
+          ><img
+            src="https://chromium.themes.zone/toolshop/wp-content/uploads/sites/14/2017/06/prod_3-300x300.jpg"
+            alt="Image placeholder"
+            class="img-fluid"
+        /></a>
+      </figure>
+      <div>
+        <p class="mb-0 item-card-category-text">Angle Grinders</p>
+        <p class="item-card-header">
+          <router-link :to="{ name: 'Details', params: { id: 12 } }">
+            SPORT BODY PARTS FO SHEVROLET 2015-2017 >
+          </router-link>
+        </p>
+        <p class="item-card-rating-wrapper">
+          <span class="fa fa-star rating-star-checked"></span>
+          <span class="fa fa-star rating-star-checked"></span>
+          <span class="fa fa-star rating-star-checked"></span>
+          <span class="fa fa-star"></span>
+          <span class="fa fa-star"></span>
+        </p>
+        <p class="font-weight-bold item-card-price">$ 50</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemCard",
+};
+</script>
